ce12001d989ea6f595351e354ba09d73024ad963
MarcoAdami <smarco.adami@gmail.com>
2025-08-26 15:46:08 +0200

    fix5

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      // Basic redirect
-      // {
-      //   source: '/',
-      //   destination: '/',
-      //   permanent: true,
-      // },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     // Basic redirect
+  //     // {
+  //     //   source: '/',
+  //     //   destination: '/',
+  //     //   permanent: true,
+  //     // },
+  //   ]
+  // },
 };
 
 module.exports = nextConfig;
